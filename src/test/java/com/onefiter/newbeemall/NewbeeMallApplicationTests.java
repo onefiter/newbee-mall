8e228de51e537958397f59816d0d9c085d4f1c8a
onefiter <onefiter@gmail.com>
2023-05-11 18:28:26 +0800

    First init

---
+++ src/test/java/com/onefiter/newbeemall/NewbeeMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onefiter.newbeemall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewbeeMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
